9a2615607cab56775d4206de7de98779a2f3c2e0
fbeh79 <behmane.f79@gmail.com>
2025-11-11 15:15:26 +0330

    UserRole

--- admin-panel/app/Providers/AppServiceProvider.php
+++ admin-panel/app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('isAdmin', function ($user) {
+           $roleName=$user->roles()->pluck('name')->toArray();
+           return in_array( 'admin',$roleName);
+        });
     }
 }
